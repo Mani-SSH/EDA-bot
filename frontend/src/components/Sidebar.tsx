126d25d661574fc240d0e860bde6dd197fd76e32
goofysoul <manishsunuwar4@gmail.com>
2024-10-22 14:46:15 +0545

    Sidebar is made contexts

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,1 +1,25 @@
+import React from "react";
+import { useSidebar } from "../contexts/SidebarContext";
+import { VscLayoutSidebarLeft } from "react-icons/vsc";
 
+const Sidebar: React.FC = () => {
+  const { isSidebarOpen, toggleSidebar } = useSidebar();
+
+  return (
+    <nav
+      className={`flex-col bg-stone-600 text-white p-4 transition-all duration-300 overflow-hidden ${
+        isSidebarOpen ? "w-1/4" : "w-20"
+      }`}
+    >
+      <div className="pb-2 flex justify-between items-center">
+        <button
+          onClick={toggleSidebar}
+          className="p-0 bg-stone-600 transition-colors duration-200 ml-auto focus:outline-none"
+        >
+          <VscLayoutSidebarLeft size={40} />
+        </button>
+      </div>
+    </nav>
+  );
+};
+export default Sidebar;
