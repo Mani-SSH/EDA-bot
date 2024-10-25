f72897f24a266b44959c8bb9971b4091ff95ef57
goofysoul <manishsunuwar4@gmail.com>
2024-10-25 20:06:40 +0545

    added all json file

--- backend/chatbot/topic_manager.py
+++ backend/chatbot/topic_manager.py
@@ -3,18 +3,33 @@ import os
 from typing import List, Optional
 
 class Topic_Manager:
-    def __init__(self, topic_dir:str = "../eda_topics/"):
-        self.topic_dir = topic_dir
+    def __init__(self):
+        self.topic_index_path = os.path.join(os.path.dirname(__file__),'..','eda_resources','topic_index.json')
         self.topic_index = self._load_topic_index()
+        self.topics = self._load_topics()
         self._cache = {}
 
-    def _load_topic_index(self)->List[str]:
-        topic_files = [
-            file_name for file_name in os.listdir(self.topic_dir)
-            if file_name.endswith('.json')
-        ]
-        return topic_files
+    def _load_topic_index(self)->dict:
+        try:
+            if not os.path.exists(self.topic_index_path):
+               raise FileExistsError(f"{self.topic_index_path} does not exists")
+           
+            with open(self.topic_index_path, 'r') as f:
+                return json.load(f)
+
+        except Exception as e:
+            print(f"Error loading knowledge base: {str(e)}")
+            return{}
+    
+    def _load_topics(self)->List[str]:
+        topics = []
+
+        for topic in self.topic_index.get("available_topics",{}).values():
+            topics.extend(topic)
+
+        return list(set(topics))
+
     
 dummy = Topic_Manager()
 
-print(dummy.topic_index)
+print(dummy.topics)
